8f1e3a5f5099380e93cb141b46c56cd1f2864ccf
basile357 <basile0707@gmail.com>
2021-06-01 17:03:05 +0300

    Updates

---
+++ static/js/8.2eed1f42.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinst=this.webpackJsonpinst||[]).push([[8],{134:function(t,n,s){"use strict";s.r(n);var c=s(2),e=s.n(c),i=s(70),u=s(10);n.default=function(){return e.a.useEffect((function(){document.title="Not Found"}),[]),Object(u.jsxs)("div",{children:[Object(u.jsx)(i.c,{}),"NotFound"]})}}}]);
+//# sourceMappingURL=8.2eed1f42.chunk.js.map
